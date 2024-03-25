e0a75195d214bc36e46b09ee3067e0c0ce35018b
linhieng <790728740@qq.com>
2024-03-25 09:28:14 +0800

    refactor: extracted multiple options as a component

---
+++ src/components/InputMultipleOne.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import InputRequired from './InputRequired.vue'
+
+const texts = defineModel<Array<string>>({ required: true })
+
+const btnAdd = (index: number) => {
+    texts.value.splice(index + 1, 0, '')
+}
+const btnRemove = (index: number) => {
+    texts.value.splice(index, 1)
+}
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <div v-for="(item, index) in texts" :key="index"
+            class="wrapper-item">
+            <div class="inner-input-s">
+                <InputRequired v-model="texts[index]" placeholder="请输入选项" />
+            </div>
+            <div class="inner-btn-s">
+                <el-button type="primary" circle
+                    class="btn"
+                    @click="() => { btnAdd(index) }">＋</el-button>
+                <el-button type="danger"
+                    :disabled="texts.length < 2" circle
+                    class="btn"
+                    @click="() => { btnRemove(index) }">一</el-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper-item {
+    margin-bottom: 10px;
+
+    display: grid;
+    grid-template-columns: 1fr max-content;
+    grid-template-rows: 1fr;
+    column-gap: 10px;
+
+    .inner-btn-s {
+        display: flex;
+        flex-wrap: wrap;
+    }
+}
+</style>
--- src/views/create/widget/SelectItem.vue
+++ src/views/create/widget/SelectItem.vue
@@ -9,6 +9,7 @@ import { getUUID, msgError } from '@/utils'
 import { ref, watch, watchEffect } from 'vue'
 import { useStoreSurvey } from '@/stores'
 import InputMultiple from '@/components/InputMultiple.vue'
+import InputMultipleOne from '@/components/InputMultipleOne.vue'
 
 const storeSurvey = useStoreSurvey()
 const survey = storeSurvey.getSurveyRef()
@@ -17,9 +18,6 @@ const props = defineProps<{
     selectType: typeof SURVEY_TYPE_SINGLE_SELECT | typeof SURVEY_TYPE_MULTI_SELECT,
     question: SurveyQuestion
 }>()
-// const emits = defineEmits<{
-//     (e: 'update', payload: SurveyQuestionContent_SingleSelect): void
-// }>()
 
 // 选项
 const selectOptions = ref([''])
@@ -37,69 +35,18 @@ watch(selectOptions.value, () => {
 if (props.question.questionContent.options) selectOptions.value = props.question.questionContent.options
 if (props.question.questionContent.titles) selectTitles.value = props.question.questionContent.titles
 
-// 添加一个选项
-const evtAddOption = (index: number) => {
-    selectOptions.value.splice(index + 1, 0, '')
-}
-// 移除一个选项
-const evtRemoveOption = (index: number) => {
-    if (selectOptions.value.length < 2) return
-    selectOptions.value.splice(index, 1)
-}
-
 </script>
 
 <template>
+    <el-divider content-position="left">请在下面输入标题内容</el-divider>
     <div>
-        <div class="outer-wrapper">
-            <InputMultiple v-model="selectTitles" />
-        </div>
-        <div class="outer-wrapper">
-            <!-- 因为这里仅仅存储字符串而已，所以可以直接使用下标作为 key -->
-            <div v-for="(item, index) in selectOptions"
-                :key="index"
-                class="mid-item">
-                <el-input
-                    v-model="selectOptions[index]" autosize type="textarea"
-                    :placeholder="'请输入选项' + (index + 1) + '的内容'"
-                    class="inner-shrink" />
-                <div class="inner-button-s">
-                    <el-button type="primary" class="btn" @click="() => { evtAddOption(index) }" circle>＋</el-button>
-                    <el-button type="danger" class="btn" :disabled="selectOptions.length < 2"
-                        @click="() => { evtRemoveOption(index) }" circle>一</el-button>
-                </div>
-            </div>
-        </div>
+        <InputMultiple v-model="selectTitles" />
+    </div>
+    <el-divider content-position="left">请在下面输入选项内容</el-divider>
+    <div>
+        <InputMultipleOne v-model="selectOptions"/>
     </div>
 </template>
 
 <style scoped lang="scss">
-.outer-wrapper {
-    .mid-item {
-        padding: 5px;
-
-        display: flex;
-        align-items: center;
-
-        >.inner-shrink {
-            flex: 1 1;
-            margin-right: 10px;
-        }
-
-        >.button-s {
-            display: flex;
-            flex-wrap: wrap;
-        }
-    }
-}
-
-.btn {
-    flex: 0 0;
-    padding: 0 5px;
-    font-size: 1rem;
-
-    &:hover {
-        box-shadow: 0 0 4px skyblue;
-    }
-}
 </style>
