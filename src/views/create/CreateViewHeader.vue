2eca84c25717c7a75ac4b76b6bec652bfd973a69
linhieng <790728740@qq.com>
2024-04-05 15:20:59 +0800

    style: make min-width when width is small

--- src/views/create/CreateViewHeader.vue
+++ src/views/create/CreateViewHeader.vue
@@ -56,6 +56,9 @@ ul {
         align-items: center;
         justify-content: center;
     }
+    li:nth-child(1) {
+        min-width: 30px;
+    }
     @include w1{
         li:nth-child(1) {
             width: 100px;
