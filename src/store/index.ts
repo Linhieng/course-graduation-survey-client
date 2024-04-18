b1228e56e32b49dc68ba4eac5d50984727c00ed1
linhieng <790728740@qq.com>
2024-04-18 22:47:35 +0800

    直接迁移统计问卷模块。删除一些无用的代码

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import useAppStore from './modules/app'
 import useUserStore from './modules/user'
 import useStoreAnswer from './modules/answer'
 import useSurveyStore from './modules/survey'
+import { useStoreStat } from './modules/stat'
 // import useTabBarStore from './modules/tab-bar'
 
 const pinia = createPinia()
@@ -12,6 +13,7 @@ export {
     useUserStore,
     useStoreAnswer,
     useSurveyStore,
+    useStoreStat,
     /* , useTabBarStore */
 }
 export default pinia
--- src/views/stat/visual/index.vue
+++ src/views/stat/visual/index.vue
@@ -1,6 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStoreStat } from '@/store'
+import AssignItem from './components/AssignItem.vue'
+import LoadingLine from '@/components/LoadingLine.vue'
+
+const storeStat = useStoreStat()
+storeStat.setSurveyId(28)
+const surveyData = storeStat.getSurveyDataRef()
+</script>
+
 <template>
     <h1>{{ $t('view.stat.visual.title') }}</h1>
+    <div class="warpper">
+        <template v-if="surveyData">
+            <p class="title">{{ surveyData.title }}</p>
+            <p class="comment">{{ surveyData.comment }}</p>
+            <ul>
+                <li v-for="q of surveyData.questions">
+                    <AssignItem :question="q" />
+                </li>
+            </ul>
+        </template>
+        <template v-else>
+            <div class="loading-box">
+                <LoadingLine />
+                <MyLoading />
+            </div>
+        </template>
+    </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.warpper {
+    padding: 20px;
+    .title {
+        font-size: 2rem;
+        margin-bottom: 10px;
+    }
+    .describe {
+        font-size: 1.4rem;
+        margin-bottom: 10px;
+    }
+}
+
+.loading-box {
+    font-size: 1.5rem;
+}
+ul {
+    list-style: none;
+}
+</style>
