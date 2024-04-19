df5cfcbfda41264c335d109cd49ea6e2857abec8
linhieng <790728740@qq.com>
2024-04-19 18:07:44 +0800

    1

--- src/views/login/locale/en-US.ts
+++ src/views/login/locale/en-US.ts
@@ -1,21 +1,30 @@
 export default {
-  'login.form.title': 'Login to Arco Design Pro',
+  'login.form.title': 'Welcome to the questionnaire system',
+  'login.form.sub-title': 'Please input information',
   'login.form.userName.errMsg': 'Username cannot be empty',
   'login.form.password.errMsg': 'Password cannot be empty',
   'login.form.login.errMsg': 'Login error, refresh and try again',
   'login.form.login.success': 'welcome to use',
-  'login.form.userName.placeholder': 'Username: admin',
-  'login.form.password.placeholder': 'Password: admin',
+  'login.form.userName.placeholder': 'Username',
+  'login.form.password.placeholder': 'Password',
   'login.form.rememberPassword': 'Remember password',
   'login.form.forgetPassword': 'Forgot password',
   'login.form.login': 'login',
   'login.form.register': 'register account',
-  'login.banner.slogan1': 'Out-of-the-box high-quality template',
-  'login.banner.subSlogan1':
-    'Rich page templates, covering most typical business scenarios',
-  'login.banner.slogan2': 'Built-in solutions to common problems',
-  'login.banner.subSlogan2':
-    'Internationalization, routing configuration, state management everything',
-  'login.banner.slogan3': 'Access visualization enhancement tool AUX',
-  'login.banner.subSlogan3': 'Realize flexible block development',
+  
+  
+  'login.banner.slogan1': 'Easy to use',
+  'login.banner.subSlogan1': 'Simple interface, one-click questionnaire generation, so that even first-time users can quickly learn to design.',
+  'login.banner.slogan2': 'Internationalization support',
+  'login.banner.subSlogan2': 'Global multi-language seamless switching, to meet cross-regional research needs, accessible global data collection.',
+  'login.banner.slogan3': 'Data analysis',
+  'login.banner.subSlogan3': 'Real-time visual charts and in-depth statistical analysis give you quick insight into the value and trends behind your data.',
+  'login.banner.slogan4': 'Page beauty',
+  'login.banner.subSlogan4': 'Refined and responsive survey design ensures a quality experience for respondents while improving survey completion rates.',
+  
+
+
+  'signup.form.password.placeholder': 'Enter password',
+  'signup.form.password.placeholder2': 'Reenter password',
+  'signup.form.userName.errMsg': 'The user name already exists',
 };
--- src/views/login/locale/zh-CN.ts
+++ src/views/login/locale/zh-CN.ts
@@ -1,19 +1,33 @@
 export default {
-  'login.form.title': '登录 Arco Design Pro',
+  'login.form.title': '欢迎登录问卷系统',
+  'login.form.sub-title': '请填写信息',
   'login.form.userName.errMsg': '用户名不能为空',
   'login.form.password.errMsg': '密码不能为空',
   'login.form.login.errMsg': '登录出错，轻刷新重试',
   'login.form.login.success': '欢迎使用',
-  'login.form.userName.placeholder': '用户名：admin',
-  'login.form.password.placeholder': '密码：admin',
+  'login.form.userName.placeholder': '用户名',
+  'login.form.password.placeholder': '密码',
   'login.form.rememberPassword': '记住密码',
   'login.form.forgetPassword': '忘记密码',
   'login.form.login': '登录',
   'login.form.register': '注册账号',
-  'login.banner.slogan1': '开箱即用的高质量模板',
-  'login.banner.subSlogan1': '丰富的的页面模板，覆盖大多数典型业务场景',
-  'login.banner.slogan2': '内置了常见问题的解决方案',
-  'login.banner.subSlogan2': '国际化，路由配置，状态管理应有尽有',
-  'login.banner.slogan3': '接入可视化增强工具AUX',
-  'login.banner.subSlogan3': '实现灵活的区块式开发',
+
+
+  'login.banner.slogan1': '简单易用',
+  'login.banner.subSlogan1': '极简操作界面，一键生成问卷，让即使是初次使用者也能迅速上手设计。',
+  'login.banner.slogan2': '国际化支持',
+  'login.banner.subSlogan2': '全球多语言无缝切换，满足跨地域研究需求，无障碍收集全球数据。',
+  'login.banner.slogan3': '数据分析',
+  'login.banner.subSlogan3': '实时可视化图表与深度统计分析功能，助您快速洞察数据背后的价值与趋势。',
+  'login.banner.slogan4': '页面美观',
+  'login.banner.subSlogan4': '精致且响应式的问卷设计，确保受访者获得优质体验，同时提升问卷填写率。',
+
+
+
+  'signup.form.password.placeholder': '输入密码',
+  'signup.form.password.placeholder2': '再次输入密码',
+  'signup.form.userName.placeholder': '请输入用户名',
+  'signup.form.userName.errMsg': '用户名不能为空',
+  'signup.form.password.errMsg': '密码不能为空',
+  'signup.form.signup.success': '注册成功',
 };
