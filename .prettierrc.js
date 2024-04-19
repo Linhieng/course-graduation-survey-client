7b9726e716a9673726d029f2aa6fb111b328588a
linhieng <790728740@qq.com>
2024-04-19 17:48:42 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 4,
+    semi: false,
+    printWidth: 120,
+    singleQuote: true,
+    quoteProps: 'consistent',
+    trailingComma: 'all',
+    htmlWhitespaceSensitivity: 'strict',
+}
