4781338e5c612ce0274f67606fbcf94cd09caf76
linhieng <790728740@qq.com>
2024-04-20 18:48:22 +0800

    sortable-demo

--- src/views/dashboard/create/components/EditSurvey.vue
+++ src/views/dashboard/create/components/EditSurvey.vue
@@ -1,17 +1,20 @@
+<script setup lang="ts">
+import QuestionList from './QuestionList.vue';
+</script>
+
 <template>
     <div class="workplace">
         <ul
             >创建问卷</ul
         >
         <h1>你好</h1>
+        <div class="list-box">
+            <QuestionList />
+        </div>
     </div>
 </template>
 
 <style scoped lang="less">
-ul {
-    width: 100%;
-    height: 2000px;
-}
 * {
     margin: 0;
     padding: 0;
---
+++ src/views/dashboard/create/components/QuestionList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Sortable from 'sortablejs';
+import { onMounted, ref } from 'vue';
+const ul = ref();
+
+onMounted(() => {
+    initSortable(ul.value);
+    Sortable.create(ul.value);
+});
+
+function initSortable(el: HTMLElement) {
+    var sortable = new Sortable(el, {
+        animation: 150, // ms, animation speed moving items when sorting, `0` — without animation
+    });
+}
+</script>
+<template>
+    <ul ref="ul" class="ul">
+        <li class="li">1</li>
+        <li class="li">2</li>
+        <li class="li">3</li>
+        <li class="li">4</li>
+        <li class="li">5</li>
+        <li class="li">6</li>
+    </ul>
+</template>
+
+<style scoped lang="less">
+.ul {
+    li {
+        padding: 20px;
+        background-color: skyblue;
+        margin: 20px;
+    }
+}
+</style>
