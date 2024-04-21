9754acd89f191ca42bc2102ab4242fb9879e7a21
linhieng <790728740@qq.com>
2024-04-21 16:45:15 +0800

    api 对问卷的基本操作

--- src/api/survey.ts
+++ src/api/survey.ts
@@ -8,6 +8,23 @@ export function cacheSurvey(data: ApiCacheSurveyData) {
     }>('/api/survey/cache', data);
 }
 
+// prettier-ignore
+export function publishSurvey(surveyId: number) { return post(`/api/survey/publish/${surveyId}`) }
+// prettier-ignore
+export function stopSurvey(surveyId: number) { return post(`/api/survey/stop/${surveyId}`) }
+// prettier-ignore
+export function delSurvey(surveyId: number) { return post(`/api/survey/del/${surveyId}`) }
+// prettier-ignore
+export function recoverSurvey(surveyId: number) { return post(`/api/survey/recover/${surveyId}`) }
+
+//
+//
+//
+//
+//
+//
+//
+//
 /** 缓存问卷，问卷不存在时自动创建 */
 export interface ApiCacheSurveyData {
     surveyId?: number;
