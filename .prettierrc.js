dc2d9ef9468ae18f178820edc08a4d613a24fc37
linhieng <linhieng@qq.com>
2024-05-26 15:50:34 +0800

    format

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+export default {
+    tabWidth: 4,
+    semi: true,
+    printWidth: 120,
+    singleQuote: true,
+    quoteProps: 'consistent',
+    trailingComma: 'all',
+    htmlWhitespaceSensitivity: 'ignore',
+};
