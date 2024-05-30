b4698b914de1f9ffc3c3cd9f4324c6501fdb0b00
linhieng <linhieng@qq.com>
2024-05-30 20:57:39 +0800

    chore: 隐藏个人网站路由页面

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -9,6 +9,7 @@ function formatModules(_modules: any, result: RouteRecordNormalized[]) {
     Object.keys(_modules).forEach((key) => {
         const defaultModule = _modules[key].default;
         if (!defaultModule) return;
+        if (defaultModule.name === 'faq') return;
         const moduleList = Array.isArray(defaultModule) ? [...defaultModule] : [defaultModule];
         result.push(...moduleList);
     });
